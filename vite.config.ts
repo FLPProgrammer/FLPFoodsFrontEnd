1236cd4994428bfc6bdf0fec686fd2b91b4a6117
Felipe <lplpfwr@gmail.com>
2026-02-22 13:59:33 -0300

    ajustes para produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path-browserify"; // Use path-browserify em vez de path
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve("src"), // Corrigido para usar o path-browserify
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
